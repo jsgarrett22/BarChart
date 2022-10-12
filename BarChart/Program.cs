0f49d13eb8b8b0a9be3f582ea9f664d8e6ba7aa1
Joshua Garrett <jgarrett35@student.cscc.edu>
2022-10-11 20:37:34 -0400

    Refactored logic structure into a calculated string given a specific index.

--- BarChart/Program.cs
+++ BarChart/Program.cs
@@ -14,49 +14,10 @@ namespace BarChart
             {
                 if (i == 10)
                 {
-                    Console.WriteLine($"{"     100: "}{CalculateBars(array[i])}");
-                } else if (i == 9)
-                {
-                    Console.WriteLine($"{"   90-99: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 8)
-                {
-                    Console.WriteLine($"{"   80-89: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 7)
-                {
-                    Console.WriteLine($"{"   70-79: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 6)
-                {
-                    Console.WriteLine($"{"   60-69: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 5)
-                {
-                    Console.WriteLine($"{"   50-59: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 4)
-                {
-                    Console.WriteLine($"{"   40-49: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 3)
-                {
-                    Console.WriteLine($"{"   30-39: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 2)
-                {
-                    Console.WriteLine($"{"   20-29: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 1)
-                {
-                    Console.WriteLine($"{"   10-19: "}{CalculateBars(array[i])}");
-                }
-                else if (i == 0)
-                {
-                    Console.WriteLine($"{"   0-9: "}{CalculateBars(array[i])}");
+                    Console.WriteLine($"{"  100: "}{CalculateBars(array[i])}");
                 } else
                 {
-                    Console.WriteLine("Invalid range.");
+                    Console.WriteLine($"{i * 10:D2}-{i * 10 + 9:D2}: {CalculateBars(array[i])}");
                 }
             }
         }
